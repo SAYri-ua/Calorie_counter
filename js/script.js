33ba92a2453ec3291891e02973a79ea89019ac96
SAYri <sayri.ua@gmail.com>
2021-04-28 01:12:52 +0300

    Adding variables

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let gender_female    = document.querySelectorAll('#gender-female'),
+    gender_male      = document.querySelector('#gender-male'),
+    age              = document.querySelector('#age'),
+    height           = document.querySelector('#height'),
+    weight           = document.querySelector('#weight'),
+    activity_minimal = document.querySelector('#activity-minimal'),
+    activity_low     = document.querySelector('#activity-low'),
+    activity_medium  = document.querySelector('#activity-medium'),
+    activity_high    = document.querySelector('#activity-high'),
+    activity_maximal = document.querySelector('#activity-maximal'),
+    submit           = document.querySelector('.form__submit-button'),
+    reset            = document.querySelector('.form__reset-button'),
+    result           = document.querySelector('.counter__result');
+
